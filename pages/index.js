7d259a5cccce9126cd945bd284fbbfaa12aa19a2
Thiago Alves Ramos Oliveira <137580515+Thiago-taroliv@users.noreply.github.com>
2025-10-09 03:28:41 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 function Home() {
-    return <h1>Oii môbem, minha linda, minha perfeita.. Te amoo muitos infinitos!!❤️❤️</h1>;
+  return (
+    <h1>
+      Oii môbem, minha linda, minha perfeita.. Te amoo muitos infinitos!!❤️❤️
+    </h1>
+  );
 }
+
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("identação errada.");
+}
+
 export default Home;
